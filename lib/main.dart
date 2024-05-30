13d05cf78448a2ca9cb786cafa6d567b3162d7bc
quinn <quinn@runsystem.net>
2024-05-30 12:05:21 +0700

    add listview item layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -37,7 +39,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
-
   Widget getPage(int index) {
     switch (index) {
       case 0:
@@ -45,7 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
       case 1:
         return const TaskPage(title: 'Completed Tasks');
       default:
-        return const TaskPage(title: 'All Tasks');
+        throw Exception('Invalid index');
     }
   }
 
@@ -73,9 +74,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: Center(
-        child: page,
-      ),
+      body: page,
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -117,12 +116,97 @@ class _TaskPageState extends State<TaskPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'This is the ${widget.title}',
-          style: const TextStyle(fontSize: 24),
+      body: ListView.builder(
+        itemCount: 30,
+        itemBuilder: (context, index) => Container(
+          padding: const EdgeInsets.only(left: 5, right: 5),
+          height: 110,
+          child: ListItem(
+            task: 'Task $index',
+            description: 'Description $index',
+          ),
         ),
       ),
+    );
+  }
+}
+
+class ListItem extends StatelessWidget {
+  final String task;
+  final String description;
+
+  const ListItem({super.key, required this.task, required this.description});
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    return Container(
+      padding: const EdgeInsets.all(3),
+      height: 120,
+      child: Card(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      task.toUpperCase(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: theme.colorScheme.primary,
+                        fontSize: 13,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      description.toUpperCase(),
+                      style: const TextStyle(fontSize: 10),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  icon: const SvgIcon(assetName: 'images/Pencil.svg'),
+                  onPressed: () => print('Edit clicked'),
+                ),
+                IconButton(
+                  icon: const SvgIcon(assetName: 'images/Trash.svg'),
+                  onPressed: () => print('Delete clicked'),
+                ),
+                IconButton(
+                  icon: const SvgIcon(assetName: 'images/CheckCircle.svg'),
+                  onPressed: () => print('Complete clicked'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SvgIcon extends StatelessWidget {
+  const SvgIcon({
+    super.key,
+    required this.assetName,
+  });
+  final String assetName;
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset(
+      assetName,
+      width: 30,
+      height: 30,
     );
   }
 }
